12f9317b5e0b538dc4c9e9984de3c0b8414162ee
robson <robson1992ferreira@hotmail.com>
2021-07-10 17:58:58 -0300

    subindo projeto

---
+++ script.js
@@ -0,0 +1,65 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count')
+const total = document.getElementById('total')
+const movieSelect = document.getElementById('movie')
+
+populateUi()
+
+let ticketPrice = movieSelect.value
+
+function setMovieData(movieIndex,moviePrice){
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrince', moviePrice);
+
+}
+function updateSelectCount(){
+  const selectedSeats = document.querySelectorAll('.row .seat.selected')
+
+  const seatsIndex = [...selectedSeats].map((seat)=>{
+    return [...seats].indexOf(seat)
+  });
+  
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
+
+
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice
+}
+
+function populateUi(){
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'))
+
+  if(selectedSeats !== null && selectedSeats.length > 0){
+    seats.forEach((seat,index) =>{
+      if(selectedSeats.indexOf(index) > -1){
+        seat.classList.add('selected')
+      }
+    })
+
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectMovieIndex')
+
+  if(selectedMovieIndex !== null ){
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
+movieSelect.addEventListener('change', (e) =>{
+  ticketPrice = e.target.value
+  setMovieData(e.target.selectedIndex, e.target.value)
+  updateSelectCount();
+})
+
+container.addEventListener('click', (e) =>{
+  if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+   e.target.classList.toggle('selected')
+    updateSelectCount();
+  }
+})
+
+updateSelectCount();
